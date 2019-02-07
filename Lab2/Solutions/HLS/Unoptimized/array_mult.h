cd5b0b2d7f9d3bbd4f110d1dfeeb015c4ae01dbe
Lenos Ioannou <l.ioannou@warwick.ac.uk>
2019-02-07 15:01:28 +0000

    Add files via upload

---
+++ Lab2/Solutions/HLS/Unoptimized/array_mult.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define SIZE 5
+#define ROWS SIZE
+#define COLS SIZE
+#define MULT_ACC SIZE
+
+#define MAX_VAL 10
+#define MIN_VAL 0
+
+void array_mult(int in_a[ROWS][COLS],
+			   				int in_b[ROWS][COLS],
+			   				int res[ROWS][COLS]);
---
+++ Lab2/Solutions/HLS/Unoptimized/array_mult_tb.c
@@ -0,0 +1,83 @@
+#include "array_mult.h"
+#include <time.h>
+
+void generate(int array[ROWS][COLS])
+{
+  int i, j;
+
+  for (i = 0; i < SIZE; i++)
+  {
+    for (j = 0; j < SIZE; j++)
+    {
+      /* populate rows and columns with rand() values */
+      array[i][j] = rand() % (MAX_VAL + 1 - MIN_VAL) + MIN_VAL;
+      printf("%d\t", array[i][j]);
+    }
+    printf("\n\r");
+  }
+  printf("\n\r");
+};
+
+void print_array(int array[ROWS][COLS]){
+  int i, j;
+
+  for (i = 0; i < SIZE; i++){
+	    for (j = 0; j < SIZE; j++){
+	      printf("%d\t", array[i][j]);
+	    }
+	    printf("\n\r");
+	  }
+	  printf("\n\r");
+};
+
+int main(){
+	int i,j,k;
+	int mult_acc;
+	int error_flag=0;
+	int in_a_tb[ROWS][MULT_ACC],in_b_tb[MULT_ACC][COLS],mult_func_exp[ROWS][COLS];
+	int mult_func_tb[ROWS][COLS];
+	time_t t;
+	t=1549294172;
+	srand((unsigned) t);//time(&t));
+	printf("Random seed: %ld\n",t);
+
+	generate(in_a_tb);
+	generate(in_b_tb);
+
+
+	array_mult(in_a_tb,in_b_tb,mult_func_tb);//calls the function in the source file
+
+
+  //calculates the expected result and makes the comparison with the one from the source file.
+	int acc_tb,acc_exp;
+	acc_exp=0;
+	ROWS_LOOP: for (i=0;i<ROWS;i++){
+		COLS_LOOP: for (j=0;j<COLS;j++){
+			mult_acc=0;
+			MULT_ACC_LOOP: for (k=0;k<MULT_ACC;k++){
+				mult_acc+=in_a_tb[i][k]*in_b_tb[k][j];
+
+			}
+			mult_func_exp[i][j]=mult_acc;
+			if (mult_func_exp[i][j]!=mult_func_tb[i][j]){
+				error_flag=1;
+			}
+		}
+	}
+
+
+	printf("Expected array:\n");
+	print_array(mult_func_exp);
+	printf("Array accelerator:\n");
+  print_array(mult_func_tb);
+
+
+	 if (error_flag==0){
+		 return 0;
+	 } else {
+		 return 1;
+	 }
+
+
+
+}
