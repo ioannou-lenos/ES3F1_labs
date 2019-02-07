26abe71f2fb52e3da6f4b7dda46a407d3a0d338d
Lenos Ioannou <l.ioannou@warwick.ac.uk>
2019-02-07 15:01:00 +0000

    Add files via upload

---
+++ Lab2/Solutions/HLS/Optimized/array_mult_p.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SIZE 5
+#define ROWS SIZE
+#define COLS SIZE
+#define MULT_ACC SIZE
+
+#define MAX_VAL 10
+#define MIN_VAL 0
+
+
+void array_mult_p(int in_a[ROWS][COLS],
+			   					int in_b[ROWS][COLS],
+			   					int res[ROWS][COLS]);
